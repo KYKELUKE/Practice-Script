fa193b1d4a17d3c4768017e9b88e891d2a726850
Enrique Luque <pari.virgoz12@gmail.com>
2024-03-16 20:57:59 -0600

    five commit

--- Practice.js
+++ Practice.js
@@ -1,6 +1,13 @@
 var nombre = "Enrique";
-var edad;
-var distrito;
-var provincia;
+var edad = 20;
+var distrito = true;
+var provincia = "deluque";
+var estatura = 1.70;
 
-alert(" Hello World this is a very Amazing " + nombre);
+console.log("Nombre: " + nombre + " " + typeof  nombre);
+console.log("Edad: " + edad + " " + typeof  edad);
+console.log("Distrito: " + distrito + " " + typeof  distrito);
+console.log("Provincia: " + provincia + " " + typeof  provincia);
+console.log("Estatura: " + estatura + " " + typeof  estatura);
+
+//alert(" Hello World this is a very Amazing " + nombre);

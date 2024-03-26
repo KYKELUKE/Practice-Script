187654c0245a14a0378f4667cd6d66d9b1f38167
Enrique Luque <pari.virgoz12@gmail.com>
2024-03-25 22:18:27 -0600

    Primer Ejercicio

--- src/Practice.js
+++
@@ -1,8 +0,0 @@
-//Operadores de Comparacion (relacionales)
-
-console.log(5>2);
-console.log(98<1);
-
-
-var numero = 9;
-console.log(4 == numero);
---
+++ src/restaurante.js
@@ -0,0 +1,10 @@
+var TotalComida = 2800 + 1300;
+var TotalBebida = 900 + 650;
+
+
+var Totaltodo = TotalComida + TotalBebida;
+
+console.log("El total de comida es: " + TotalComida)
+console.log("El total de bebida es: " + TotalBebida)
+console.log("El total de la cuenta es: " + Totaltodo)
+

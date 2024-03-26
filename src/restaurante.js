b713e6830c99fb474ae3d74cf382facbc65e4479
Enrique Luque <pari.virgoz12@gmail.com>
2024-03-25 22:57:41 -0600

    Aprendiendo if y else

--- src/restaurante.js
+++ src/restaurante.js
@@ -1,10 +1,11 @@
-var TotalComida = 2800 + 1300;
-var TotalBebida = 900 + 650;
+var Aprobe = true;
+
+if(Aprobe == true){
+    console.log("Puedo salir!!!")
 
 
-var Totaltodo = TotalComida + TotalBebida;
-
-console.log("El total de comida es: " + TotalComida)
-console.log("El total de bebida es: " + TotalBebida)
-console.log("El total de la cuenta es: " + Totaltodo)
+}
+else{
+    console.log("Tengo que estudiar :,(((")
+}
 
